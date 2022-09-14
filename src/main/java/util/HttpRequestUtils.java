89dc45b0ef934ded16c6ec03f90ab1aaf6233d19
Jaehui-Lee <tkdoqr1234@gmail.com>
2022-09-14 16:45:38 +0900

    Static Html 응답 구현

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -8,6 +8,11 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
+    public static String getUrl(String firstLine) {
+        String[] splitted = firstLine.split(" ");
+        String url = splitted[1];
+        return url;
+    }
     /**
      * @param queryString URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
