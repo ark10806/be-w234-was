093d6a250a95fc3e58b7b13e0b27622b892e6ac6
cocobol <coco.bol@kakaocorp.com>
2022-09-14 17:37:30 +0900

    현재 컨트롤러 역할을 하는 RequestHandler 코드를 좀더 다른분들이 보기 쉽게 정리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -26,24 +26,21 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // 요청 패킷 파싱
             HttpRequest requestData = new SimpleHttpRequest(in);
-            logger.info("http request parsed");
 
-            DataOutputStream dos = new DataOutputStream(out);
-
-//            byte[] body = "Hello World".getBytes();
-//            response200Header(dos, body.length);
-//            responseBody(dos, body);
-
+            // 파일 준비
             String path = requestData.getPath();
-            logger.info("PATH {}",path);
             FileResponseBody fileResponseBody = new FileResponseBody("webapp/" + path);
-            logger.info("file response body created");
+
+            // 응답 객체 만들기 (빌더 패턴 썼으면 더 좋았을까 하는 고민 살짝,,)
             HttpResponse response = new BaseHTTPResponse()
                     .status(200)
                     .addHeader(new ResponseHeader("Content-Type", "text/html;charset=utf-8"))
                     .body(fileResponseBody);
-            logger.info("http response prepread");
+
+            // 응답 전송
+            DataOutputStream dos = new DataOutputStream(out);
             response.send(dos);
 
         } catch (IOException e) {
@@ -61,24 +58,4 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.write("HTTP/1.1 200 OK \r\n".getBytes());
-            dos.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
-            dos.write(("Content-Length: " + lengthOfBodyContent + "\r\n").getBytes());
-            dos.write("\r\n".getBytes());
-        } catch (IOException e) {
-            logger.info("response200Header() error");
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 }
