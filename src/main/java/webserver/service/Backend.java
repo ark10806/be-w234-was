dbf7cd61ebf18a6f29df37281f489ef9375d58ed
eigen.vector <eigen.vector@kakaocorp.com>
2022-09-23 17:25:39 +0900

    [update]

--- src/main/java/webserver/service/Backend.java
+++ src/main/java/webserver/service/Backend.java
@@ -2,6 +2,7 @@ package webserver.service;
 
 import http.RequestPacket;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -49,9 +50,12 @@ public class Backend {
     private String routeView(String url) {
         try {
             return Files.readString(new File(rootDir + url).toPath());
+        } catch (FileNotFoundException e) {
+            logger.error("routeView: {}", e.getMessage());
+            return HttpStatus.NOT_FOUND.getMessage();
         } catch (IOException e) {
-            logger.debug("routeView: {}", e.getMessage());
-            return "404 not found";
+            logger.error("routeView: {}", e.getMessage());
+            return HttpStatus.INTERNAL_SERVER_ERROR.getMessage();
         }
     }
 }
